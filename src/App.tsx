428f4b09e9949234da8e00d6501c1aafef34412c
georgia.antunes <georgiaantunes@outlook.com>
2024-05-04 15:49:18 -0300

    rotas App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
+import { Outlet } from "react-router-dom"
 function App() {
   return (
     <>
       <div className='App'>
+        <Outlet/>
         <h1>Github Finder</h1>
       </div>
     </>
